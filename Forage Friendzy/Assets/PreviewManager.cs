38d70db0036aaeb5c898a72345034cc2fb4f2277
SebastianSusini <sebastiansusini@gmail.com>
2023-03-29 18:50:32 -0400

    Cosmetic Swapper Functions

---
+++ Forage Friendzy/Assets/PreviewManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PreviewManager : MonoBehaviour
+{
+
+    public static PreviewManager Instance { get; private set; }
+
+    [SerializeField] private List<PreviewObject> availablePreviewObjects;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instance = this;
+    }
+
+    public PreviewObject GetPreviewObject()
+    {
+        return availablePreviewObjects.Find(x => x.IsLoaned());
+    }
+}
