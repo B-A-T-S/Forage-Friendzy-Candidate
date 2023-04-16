c3f55c9aa7188b7255d258119357aa47c8338862
Jared Scott <57363461+JScott1997@users.noreply.github.com>
2023-04-16 18:02:00 -0400

    Controller Menu Nav & Conveyance

--- Forage Friendzy/Assets/Scripts/UI/ControllerMenuNavigation.cs
+++ Forage Friendzy/Assets/Scripts/UI/ControllerMenuNavigation.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
@@ -8,6 +9,10 @@ public class ControllerMenuNavigation : MonoBehaviour
 {
     private bool controllerConnected;
     [SerializeField] private Selectable selectOnConnection;
+    [SerializeField] private GameObject joinButtons;
+    [SerializeField] private Button hostButton;
+    [SerializeField] private Button backButton;
+    [SerializeField] private bool updateNav;
     string[] controllers;
 
     private void Start()
@@ -15,6 +20,13 @@ public class ControllerMenuNavigation : MonoBehaviour
         controllers = null;
         StartCoroutine(CheckForControllers());
     }
+
+    //private void Update()
+    //{
+    //    if (updateNav) { 
+    //        JoinLobbyNavigation();
+    //    }
+    //}
 
     IEnumerator CheckForControllers()
     {
@@ -50,5 +62,46 @@ public class ControllerMenuNavigation : MonoBehaviour
     {
         if(controllerConnected)
             button.Select();
+    }
+
+    public void JoinLobbyNavigation()
+    {
+        for(int i = 0; i < joinButtons.transform.childCount; i++)
+        {
+            GameObject currentLobby = joinButtons.transform.GetChild(i).gameObject;
+            Navigation nav = new Navigation();
+            Navigation hostNav = new Navigation();
+            nav.mode = Navigation.Mode.Explicit;
+            if (i == 0)
+            {
+                nav.selectOnUp = hostButton;
+                hostNav.selectOnUp = currentLobby.GetComponent<Selectable>();
+            }
+            else
+                nav.selectOnUp = joinButtons.transform.GetChild(i - 1).gameObject.GetComponent<Selectable>();
+
+            if ((i + 1) >= joinButtons.transform.childCount)
+            {
+                nav.selectOnDown = hostButton;
+                hostNav.selectOnDown = currentLobby.GetComponent<Selectable>();
+            }
+            else
+                nav.selectOnDown = joinButtons.transform.GetChild(i + 1).gameObject.GetComponent<Selectable>();
+
+            hostButton.navigation = hostNav;
+            currentLobby.GetComponent<Button>().navigation = nav;
+        }
+    }
+
+    public void InsertButtonNavigation(Selectable addedSelectable, Selectable left = null, Selectable right= null, Selectable up = null, Selectable down= null)
+    {
+        if (up != null)
+        {
+            Navigation navAdded = new Navigation();
+            Navigation navExisting = new Navigation();
+            navAdded.mode = Navigation.Mode.Explicit;
+            navExisting.mode = Navigation.Mode.Explicit;
+
+        }
     }
 }
